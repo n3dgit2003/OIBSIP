154131a0968bac05054468365934298f07481f02
Noel Biju Daniel <noelbijudaniel2003@gmail.com>
2026-03-13 08:56:34 +0530

    Create script.js

---
+++ Task3_Temperature_Converter/script.js
@@ -0,0 +1,30 @@
+function convertTemp(){
+
+let temp = document.getElementById("temperature").value;
+let unit = document.getElementById("unit").value;
+let result = document.getElementById("result");
+
+if(temp === ""){
+result.innerHTML = "Please enter a temperature";
+return;
+}
+
+temp = Number(temp);
+
+if(unit === "celsius"){
+
+let fahrenheit = (temp * 9/5) + 32;
+
+result.innerHTML = temp + " °C = " + fahrenheit.toFixed(2) + " °F";
+
+}
+
+else{
+
+let celsius = (temp - 32) * 5/9;
+
+result.innerHTML = temp + " °F = " + celsius.toFixed(2) + " °C";
+
+}
+
+}
